cdfa6e8235cf7f98afeb0836d6da42e122744443
csmanel <csmanel@gmail.com>
2024-01-30 14:10:26 -0500

    fix import on tracks

--- frontend/src/components/trackshow/TrackShow.jsx
+++ frontend/src/components/trackshow/TrackShow.jsx
@@ -1,14 +1,13 @@
-import AudioResponse from "../audioResponse/AudioResponse";
-import TrackMasterDisplay from "../TrackMasterDisplay/TrackMasterDisplay";
-import "./trackShow.css";
-
+import AudioResponse from '../audioResponse/AudioResponse';
+import TrackMasterDisplay from '../trackMasterDisplay/TrackMasterDisplay';
+import './trackShow.css';
 
 const TrackShow = () => {
   return (
     <div className="track-show-page">
       <div className="left-container">
         <div className="master-project">
-            <TrackMasterDisplay/>
+          <TrackMasterDisplay />
         </div>
         <div className="audio-responses-container">
           <AudioResponse />
