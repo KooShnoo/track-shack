bcd225626ab3b4a236482b90e75b19c4a829bb82
David <dmaine6711@gmail.com>
2024-01-30 09:32:09 -0500

    create component show page

---
+++ frontend/src/components/trackshow/TrackShow.jsx
@@ -0,0 +1,16 @@
+import "./trackShow.css";
+
+const TrackShow = () => {
+  return (
+    <div className="track-show-page">
+      <div className="left-container">
+        <div className="master-project"></div>
+        <div className="audio-responses-container">
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TrackShow;
