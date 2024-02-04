71c20cdd662e2327a7c85b2c13875c751cbcdfdb
Jacob Tabor <jtabor214@gmail.com>
2024-02-04 17:38:36 -0500

    Remove excess text in CommentThread.jsx

--- frontend/src/components/comment/CommentThread/CommentThread.jsx
+++ frontend/src/components/comment/CommentThread/CommentThread.jsx
@@ -29,7 +29,6 @@ const CommentThread = ({ id, author, timeAgo, content }) => {
             dispatch(deleteComment(id, trackId));
           }}
         >
-          DELETE
         </p>
       )}
 
