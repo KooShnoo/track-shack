01fb6a7711bb054a73041b59cb23c59efd20b0ed
David <dmaine6711@gmail.com>
2024-02-07 17:35:29 -0500

    resize comments, add divs to separate the create comment section from comments scroll

--- frontend/src/components/commentsContainer/CommentsContainer.jsx
+++ frontend/src/components/commentsContainer/CommentsContainer.jsx
@@ -1,18 +1,34 @@
-import Comment from '../comment/Comment';
-import './commentsContainer.css';
-import CreateComment from '../createComment/CreateComment';
-import { useSelector } from 'react-redux';
+import Comment from "../comment/Comment";
+import "./commentsContainer.css";
+import CreateComment from "../createComment/CreateComment";
+import { useSelector } from "react-redux";
 
-const CommentsContainer = ({trackId}) => {
-  const loggedIn = useSelector((state) => !!state.session.user)
+const CommentsContainer = ({ trackId }) => {
+  const loggedIn = useSelector((state) => !!state.session.user);
 
   // const track = useSelector(state => state.trackPosts[trackId])
-  const comments = useSelector(state => state.trackPosts[trackId]?.comments)
+  const comments = useSelector((state) => state.trackPosts[trackId]?.comments);
 
   return (
     <div className="commentsContainer">
-      {loggedIn ? <CreateComment /> : <p style={{fontWeight: 'bold', fontSize: '2rem', textAlign: 'center'}}>sign in to comment!</p>}
-      <Comment comments={comments} />
+      <div className="create">
+        {loggedIn ? (
+          <CreateComment />
+        ) : (
+          <p
+            style={{
+              fontWeight: "bold",
+              fontSize: "2rem",
+              textAlign: "center",
+            }}
+          >
+            sign in to comment!
+          </p>
+        )}
+      </div>
+      <div className="comments-scroll">
+        <Comment comments={comments} />
+      </div>
     </div>
   );
 };
