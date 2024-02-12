e3811d1e798568b8aeec5c389a3dc87645a1dbc1
Jacob Tabor <jtabor214@gmail.com>
2024-02-12 16:50:16 -0500

    Build UserProfileShow.jsx and UserProfileShow.css

---
+++ frontend/src/components/Profile/UserProfileShow.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import "./UserProfileShow.css";
+
+const UserProfileShow = ({tracks, comments}) => {
+
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.session.user) || null;
+  const { userId } = useParams;
+  // const tracks = user.tracks;
+  // const comments = user.comments;
+  
+  const [showEditProfile, setShowEditProfile] = useState(false);
+  const [username, setUsername] = useState(user.username);
+  const [profileEmail, setProfileEmail] = useState(user.email);
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
+  
+  const [profilePicture, setProfilePicture] = useState(null);
+  const [bio, setBio] = useState('');
+  
+  useEffect(() => {
+    if ( userId ) {
+      dispatch(fetchUserTracks(userId, tracks));
+    }
+  }, [userId, tracks, dispatch]);
+
+  useEffect(() => {
+    if ( userId ) {
+      dispatch(fetchUserComments(userId, comments));
+    }
+  }, [userId, comments, dispatch]);
+
+  return (
+    <>
+      <div className="profile-main-page">
+        <div className="profile-info-container">
+          <div id="profile-picture"><i className="fa-solid fa-compact-disc fa-spin"></i></div>
+          <div id="profile-info">
+            <h1>{user.username}</h1>
+            <p>{user.email}</p>
+            <p>{bio}</p> 
+          </div>
+          <button onClick={() => setShowEditProfile(!showEditProfile)}>Edit Info</button>
+        </div>
+        {showEditProfile && 
+        <div className="edit-info-form-container">
+          <button type="submit" onClick={setShowEditProfile(false)}>Close</button>
+          <form className="edit-profile-info">
+            <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <input type="text" placeholder="Email" value={profileEmail} onChange={(e) => setProfileEmail(e.target.value)} />
+            <textarea placeholder="Tell us about you!..." id="bio-field" value={bio} onChange={(e) => setBio(e.target.value)}></textarea>
+            <input type="password" placeholder="Change Password" onChange={(e) => (setNewPassword(e.target.value) && setPasswordsMatch(e.target.value === confirmPassword))}/>
+            <input type="password" placeholder="Confirm New Password" onChange={(e) =>(setConfirmPassword(e.target.value) && setPasswordsMatch(e.target.value === newPassword))}/>
+            <button type="submit">Update Info</button>
+          </form>
+        </div>
+        }
+      </div>
+    </>
+  );
+};
