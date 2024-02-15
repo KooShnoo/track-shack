4f303ded82b5d1ae86ace0989b1cbb54499c8ac6
Cole Manel <csmanel@gmail.com>
2024-02-15 11:44:46 -0500

    fix login styling page

--- frontend/src/components/SessionForms/LoginForm.jsx
+++ frontend/src/components/SessionForms/LoginForm.jsx
@@ -1,16 +1,16 @@
-import { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import "./LoginForm.css";
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import './LoginForm.css';
 
-import { login, clearSessionErrors } from "../../store/session";
+import { login, clearSessionErrors } from '../../store/session';
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const errors = useSelector((state) => state.errors.session);
   const dispatch = useDispatch();
 
-  if (errors) console.log("HIHIHI", errors);
+  if (errors) console.log('HIHIHI', errors);
 
   useEffect(() => {
     return () => {
@@ -19,7 +19,7 @@ function LoginForm() {
   }, [dispatch]);
 
   const update = (field) => {
-    const setState = field === "email" ? setEmail : setPassword;
+    const setState = field === 'email' ? setEmail : setPassword;
     return (e) => setState(e.currentTarget.value);
   };
 
@@ -33,52 +33,53 @@ function LoginForm() {
 
     return dispatch(
       login({
-        email: "demo@hello.com",
-        password: "password",
+        email: 'demo@hello.com',
+        password: 'password',
       })
     );
   };
 
-  if (errors) console.log("ERRORS", errors);
+  if (errors) console.log('ERRORS', errors);
   return (
-    <form className="login-form" onSubmit={handleSubmit}>
-      {/* <div className="errors">{errors.errors}</div> */}
-      <h2 id="login-header">Login</h2>
-      <div className="credential-fields">
-        <div className="errors">{errors?.errors?.email}</div>
-        <label>
-          <span>Email</span>
+    <div className="signup-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h2 id="login-header">Login</h2>
+        <div className="credential-fields">
+          <div className="errors">{errors?.errors?.email}</div>
+          <label>
+            <span>Email: </span>
+            <input
+              id="input-field"
+              type="text"
+              value={email}
+              onChange={update('email')}
+              placeholder="Email"
+            />
+          </label>
+          <div className="errors">{errors?.message}</div>
+          <label>
+            <span>Password: </span>
+            <input
+              id="input-field"
+              type="password"
+              value={password}
+              onChange={update('password')}
+              placeholder="Password"
+            />
+          </label>
+          <br />
           <input
-            id="input-field"
-            type="text"
-            value={email}
-            onChange={update("email")}
-            placeholder="Email"
+            id="submit-button"
+            type="submit"
+            value="Log In"
+            disabled={!email || !password}
           />
-        </label>
-        <div className="errors">{errors?.message}</div>
-        <label>
-          <span>Password</span>
-          <input
-            id="input-field"
-            type="password"
-            value={password}
-            onChange={update("password")}
-            placeholder="Password"
-          />
-        </label>
-        <br />
-        <input
-          id="submit-button"
-          type="submit"
-          value="Log In"
-          disabled={!email || !password}
-        />
-        <button id="demo-button" type="submit" onClick={handleDemoLogin}>
-          Demo User
-        </button>
-      </div>
-    </form>
+          <button id="demo-button" type="submit" onClick={handleDemoLogin}>
+            Demo User
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
